ecac471e5ff6b82b98266064e04429b88cb916e8
d3xt3r0 <prathapss87@gmail.com>
2022-09-10 16:14:31 +0530

    Add function to return intensity matrix

--- main.py
+++ main.py
@@ -7,11 +7,45 @@ def read_image(loc):
     try:
         image = Image.open(loc)
         width, height = image.size
-        print(f"Width = {width}\nHeight = {height}")
+        print(f"Successfully loaded the image!\nImage Size: {width} x {height}\n")
+
     except FileNotFoundError as error:
         print(error)
 
+    # convert the image as a 2D array
+
+    pixel_data = list(image.getdata())
+
+    pixel_matrix = []
+    
+    for i in range(0,len(pixel_data),image.width):
+        pixel_matrix.append(pixel_data[i : i + image.width])
+
+    return pixel_matrix
+
+
+def get_intensity_matrix(pixel_matrix):
+
+    '''
+    Convert the RGB values of each pixel to a single brightenss value.
+
+    '''
+
+    intensity_matrix = []
+
+    for row in pixel_matrix:
+        temp = []
+        for value in row:
+            avg = value[0] + value[1] + value[2] / 3
+            temp.append(avg)
+        intensity_matrix.append(temp)
+
+    return intensity_matrix
+
+
+
 
 if __name__ == "__main__":
-    print("Welcome to ascii-art-generator!")
-    read_image("res/ascii-pineapple.jpg")
+    print("Welcome to ascii-art-generator!\n")
+    pixel_matrix = read_image("res/ascii-pineapple.jpg")
+    intensity_matrix = get_intensity_matrix(pixel_matrix)
